419c5e2017e1c6aeaa207ea985fee775f04fed6a
LuaxY <yann.guineau@voidmx.net>
2014-12-31 18:11:17 +0100

    Add page 404 not found

---
+++ app/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('header')
+    {{ HTML::style('css/404.css') }}
+@stop
+
+<?php $no_carousel = true; ?>
+
+@section('page')
+    <div class="container">
+        <div class="p404">
+            <div class="p404-text">
+                <div>404</div>
+                <div>Tous les chemins mènent au monde des douze...</div>
+                <div>sauf celui-là !</div>
+            </div>
+            <a class="p404-back" href="/">Aller à l'accueil</a>
+        </div>
+    </div>
+@stop
