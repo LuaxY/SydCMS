e29c139468a0c2506c23ca377af3103d7efe2638
LuaxY <yann.guineau@voidmx.net>
2015-02-15 13:47:22 +0100

    Move vote object into vote palier

--- app/views/vote/palier.blade.php
+++ app/views/vote/palier.blade.php
@@ -17,7 +17,30 @@
 @endforeach
                             </div>
                             <div class="vote-item mask-relative masked">
-@include('vote.object')
+                                <div class="vote-gift-details vote-block-{{ $current }}">
+                                    <div class="vote-gift-title-block">
+                                        <span class="vote-reward-text">
+                                            <span></span>
+                                            votes
+                                        </span>
+                                        <div class="vote-gift-title">
+                                            <span class="vote-gift-title-next">Cadeau à obtenir :</span>
+                                            <span class="vote-gift-title-object"></span>
+                                        </div>
+                                    </div>
+                                    <div class="vote-gift-description-block">
+                                        <div class="object-illu left">
+                                            <img src="" />
+                                        </div>
+                                        <div class="vote-gift-description right">
+                                            <div class="title">
+                                                <span class="picto"></span>
+                                                Description
+                                            </div>
+                                            <p></p>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div class="loadmask" style="display: block;"></div>
                                 <div class="loading" style="display: block; top: 100px;"></div>
                             </div>
